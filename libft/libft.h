7756002cc4ec4f6b4c34ebbaee77e357f4f625fc
Orwa Zain <orwazain3@gmail.com>
2026-04-16 17:34:38 +0200

    c type functions

---
+++ libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zorwa <zorwa@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 13:19:13 by zorwa             #+#    #+#             */
+/*   Updated: 2026/04/16 17:22:55 by zorwa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
